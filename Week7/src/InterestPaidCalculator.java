2e673fe21da005374c2d892625b84a9b7b51ce73
TransientCW <chris@myubercode.com>
2017-06-22 06:54:56 -0600

    working on week 7

--- Week7/src/InterestPaidCalculator.java
+++ Week7/src/InterestPaidCalculator.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
@@ -9,16 +10,37 @@ public class InterestPaidCalculator {
 
     public static void main(String[] args) {
         final File outputFileName = new File("report.txt");
+        String inputTextFileName;
         File inputFile = null;
         Scanner inputScanner = null;
+        Scanner keyboardReader = new Scanner(System.in);
         File outputFile = null;
         PrintWriter printWriter = null;
-        String inputTextFileName = "";
         boolean canOpenInputFile = false;
         boolean canOpenOutputFile = false;
-        Scanner keyboardReader = new Scanner(System.in);
         int numAccountsProcessed = 0;
         double totalInterestPaidAnnual = 0.0D;
-        System.out.println("This program will......");
+
+        System.out.println("This program will calculate the interest paid" +
+                "by an investment company,\n from all accounts, over " +
+                "the course of one year.");
+        System.out.println();
+        System.out.println("Enter input data filename (inlcuding .txt):");
+        inputTextFileName = keyboardReader.next();
+        System.out.println("filename: " + inputTextFileName);
+        System.out.println();
+        System.out.println("Reading file and calculating interest.....");
+
+        try {
+            inputFile = new File(inputTextFileName);
+            //System.err.println(inputFile.getAbsolutePath());
+            inputScanner = new Scanner(inputFile);
+            while(inputScanner.hasNextLine()) {
+                System.out.println(inputScanner.nextLine());
+            }
+
+        } catch(Exception e) {
+            System.out.println("error");
+        }
     }
 }
